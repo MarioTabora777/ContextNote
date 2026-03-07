648d35958f70898aba866adddf629d939cf68115
Daniel Marquez <65589229+danielm488@users.noreply.github.com>
2026-03-06 19:43:39 -0600

    se agrego la foto en el fondo de pantalla

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, Text, StyleSheet, Alert, ImageBackground } from "react-native";
 import CustomInput from "../../components/CusstomInput";
 import CustomButton from "../../components/CustomButton";
 import { useAuth } from "../../context/AuthContext";
@@ -32,7 +32,12 @@ export default function LoginScreen({ navigation }: Props) {
   };
 
   return (
-    <View style={styles.container}>
+    <ImageBackground 
+      source={ require( '../../Images/world-map-detailed.jpg') } 
+      style={styles.Imagecontainer}
+      resizeMode="cover"
+    >
+    {<View style={styles.container}>
       <Text style={styles.title}>Iniciar sesión</Text>
 
       <CustomInput
@@ -58,11 +63,14 @@ export default function LoginScreen({ navigation }: Props) {
         variant="secondary"
         onPress={() => navigation.navigate("Register")}
       />
-    </View>
+    </View>}
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 18, justifyContent: "center" },
+  
+  Imagecontainer: {flex:1, width:'100%',height:'100%'},
+  container: { flex: 1, marginTop:200,marginBottom:250 ,padding: 18, justifyContent: "center", backgroundColor:'#ffffffcf' },
   title: { fontSize: 22, fontWeight: "700", marginBottom: 20, textAlign: "center" },
 });
